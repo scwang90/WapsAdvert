845b348b10caedb3dd075d29eff48a7065b0f2c3
scwang90 <scwang90@hotmail.com>
2016-08-30 00:02:18 +0800

    在线部署使用新的框架

---
+++ src/main/java/com/example/scwang/appxadvert/AppxAdapter.java
@@ -0,0 +1,126 @@
+package com.example.scwang.appxadvert;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.andadvert.AdvertAdapter;
+import com.andadvert.OnlineKey;
+import com.andadvert.model.AdCustom;
+import com.andframe.application.AfApplication;
+import com.andframe.util.java.AfStringUtil;
+import com.baidu.appx.BDNativeAd;
+import com.wpadvert.kernel.WpAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 万普广告适配器
+ * @author 树朾
+ */
+public class AppxAdapter extends WpAdapter {
+
+    private static String APPX_APP_KEY = "wVs28LdYEBjDO3mfq9mZOfBBbsguGAKq";
+    private static String APPX_NATIVE_KEY = "wVs28LdYEBjDO3mfq9mZOfBBbsguGAKq";
+
+    private BDNativeAd nativeAd = null;
+    private Activity nativeActivity = null;
+    /**
+     * 获取全局 广告适配器
+     */
+    public static WpAdapter getAppxInstance(){
+        return AfApplication.getApp().getSingleton(AdvertAdapter.KEY_ADVERT);
+    }
+
+    public AppxAdapter(String defchannel) {
+        super(defchannel);
+    }
+
+    public static void initialize(AfApplication application, String channel, String currency, String appId) {
+        APP_ID = appId;
+        UNIT_POINT = AfStringUtil.isNotEmpty(currency)?currency:UNIT_POINT;
+        application.setSingleton(AdvertAdapter.KEY_ADVERT, new AppxAdapter(channel));
+    }
+
+    public static void initialize(String APPX_APP_KEY, String APPX_NATIVE_KEY) {
+        AppxAdapter.APPX_APP_KEY = APPX_APP_KEY;
+        AppxAdapter.APPX_NATIVE_KEY = APPX_NATIVE_KEY;
+    }
+
+
+    @Override
+    public void initInstance(Context context) {
+        super.initInstance(context);
+        if (IS_WAPSWORKS && context instanceof Activity) {
+            if (nativeAd == null || nativeActivity != context) {
+                if (nativeAd != null) {
+                    nativeAd.destroy();
+                }
+                nativeActivity = (Activity) context;
+                nativeAd = new BDNativeAd(nativeActivity, APPX_APP_KEY, APPX_NATIVE_KEY);
+                nativeAd.loadAd();
+            }
+        }
+    }
+
+    @Override
+    public AdCustom getAdCustom(Context context) {
+        if (IS_WAPSWORKS) {
+            return super.getAdCustom(context);
+        }
+        return null;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<AdCustom> getAdCustomList(Context context) {
+        List<AdCustom> list = new ArrayList<>();
+        if (IS_WAPSWORKS) {
+            UNIT_PRICE = OnlineKey.getInteger(context, OnlineKey.KEY_UNITPRICE, UNIT_PRICE, "get unitprice");
+            ArrayList<BDNativeAd.AdInfo> infos = nativeAd.getAdInfos();
+            for (BDNativeAd.AdInfo info : infos) {
+                list.add(new AppxNative(info, UNIT_PRICE));
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public void showDetailAd(Context context, AdCustom adinfo) {
+        if (IS_WAPSWORKS) {
+            if (adinfo instanceof AppxNative) {
+                ((AppxNative) adinfo).didShow();
+            } else {
+                super.showDetailAd(context, adinfo);
+            }
+        }
+    }
+
+    @Override
+    public void downloadAd(Context context, AdCustom adinfo) {
+        if (IS_WAPSWORKS) {
+            if (adinfo instanceof AppxNative) {
+                ((AppxNative) adinfo).didClick();
+            } else {
+                super.downloadAd(context, adinfo);
+            }
+        }
+    }
+
+    @Override
+    public void uninstallAd(Context context) {
+        super.uninstallAd(context);
+        if (IS_WAPSWORKS) {
+            if (nativeAd != null) {
+                nativeAd.destroy();
+                nativeAd = null;
+            }
+        }
+    }
+
+    @Override
+    public void showOffers(Context context) {
+        super.showOffers(context);
+    }
+
+}
---
+++ src/main/java/com/example/scwang/appxadvert/AppxNative.java
@@ -0,0 +1,38 @@
+package com.example.scwang.appxadvert;
+
+import com.andadvert.model.AdCustom;
+import com.andframe.application.AfApplication;
+import com.baidu.appx.BDNativeAd;
+
+/**
+ * AppxNative
+ * Created by SCWANG on 2016/8/29.
+ */
+@SuppressWarnings("unused")
+public class AppxNative extends AdCustom {
+
+    private final BDNativeAd.AdInfo info;
+
+    public AppxNative(BDNativeAd.AdInfo info, int unitPrice) {
+        this.info = info;
+        this.Points = unitPrice;
+        this.IconUrl = info.getIconUrl();
+        this.Name = info.getTitle();
+        this.Description = info.getDescription();
+        this.Action = "安装";
+        this.Filesize = info.getFileSize();
+        this.Id = info.getClickUrl();
+        this.Package = AfApplication.getApp().getPackageName();
+        this.ImageUrls = new String[]{
+            info.getImageUrl()
+        };
+    }
+
+    public void didShow() {
+        info.didShow();
+    }
+
+    public void didClick() {
+        info.didClick();
+    }
+}
